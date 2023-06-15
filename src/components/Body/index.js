ed58ef5e4f2a590c5ba0a1a4263a3398ea19781b
ganireddy651 <gajarla.ganesh1995@gmail.com>
2023-06-15 07:51:06 +0000

    cvqw

--- src/components/Body/index.js
+++ src/components/Body/index.js
@@ -1,1 +1,39 @@
-// Write your code here
+import './index.css'
+
+const Body = () => (
+  <div className="body-container">
+    <div className="left-navbar-container">
+      <h1 className="left-nav-heading">Left Navbar Menu</h1>
+      <ul className="list-container">
+        <li className="list">Item 1</li>
+        <li className="list">Item 2</li>
+        <li className="list">Item 3</li>
+        <li className="list">Item 4</li>
+      </ul>
+    </div>
+    <div className="content-navbar-container">
+      <h1 className="content-heading">Content</h1>
+      <p>
+        Go back and read the content marketing definition one more time, but
+        this time remove the relevant and valuable. That’s the difference
+        between content marketing and the other informational garbage you get
+        from companies trying to sell you “stuff.” Companies send us information
+        all the time – it’s just that most of the time it’s not very relevant or
+        valuable (can you say spam?). That’s what makes content marketing so
+        intriguing in today’s environment of thousands of marketing messages per
+        person per day.
+      </p>
+    </div>
+    <div className="right-navbar-container">
+      <h1 className="left-nav-heading">Right Navbar</h1>
+      <ul className="right-navbar-ad-container">
+        <li className="ad">Ad 1</li>
+      </ul>
+      <ul className="right-navbar-ad-container">
+        <li className="ad">Ad 2</li>
+      </ul>
+    </div>
+  </div>
+)
+
+export default Body
--- src/components/ConfigurationController/index.js
+++ src/components/ConfigurationController/index.js
@@ -1,1 +1,32 @@
-// Write your code here
+import {Component} from 'react'
+import './index.css'
+
+class ConfigurationController extends Component {
+  render() {
+    return (
+      <div className="config-container">
+        <h1 className="config-heading">Layout</h1>
+        <div className="checkbox-container">
+          <input className="input" type="checkbox" id="content" />
+          <label className="label" htmlFor="content">
+            Content
+          </label>
+        </div>
+        <div className="checkbox-container">
+          <input className="input" type="checkbox" id="leftNavbar" />
+          <label className="label" htmlFor="leftNavbar">
+            Left Navbar
+          </label>
+        </div>
+        <div className="checkbox-container">
+          <input className="input" type="checkbox" id="rightNavbar" />
+          <label className="label" htmlFor="rightNavbar">
+            Right Navbar
+          </label>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default ConfigurationController
